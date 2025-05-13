3c090ef866b9183ab053cc0956f83dbdeb356efa
Artin <artinazod85@gmail.com>
2025-05-13 14:55:27 +0330

    player1 idle and player1 dash addded

--- Assets/Scripts/Player1controll.cs
+++ Assets/Scripts/Player1controll.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Scripting.APIUpdating;
@@ -6,10 +7,22 @@ public class Player1controll : MonoBehaviour
 {
     [SerializeField] private float speed = 3.5f;
     private Vector2 movingInput;
+    private Boolean inDash = false;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         movingInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnDash(InputAction.CallbackContext context) {
+        if (context.performed)
+        {
+            inDash = true;
+        }
+        else if (context.canceled)
+        {
+            inDash = false;
+        }
     }
 
     public void Update()
@@ -19,6 +32,7 @@ public class Player1controll : MonoBehaviour
 
     private void move()
     {
+        float MoveSpeed = inDash ? speed * 2 : speed;
         //character direction
         if (movingInput.x > 0)
         {
@@ -28,7 +42,7 @@ public class Player1controll : MonoBehaviour
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
-        Vector2 move = new Vector2(movingInput.x, 0) * speed * Time.deltaTime;
+        Vector2 move = new Vector2(movingInput.x, 0) * MoveSpeed * Time.deltaTime;
         transform.Translate(move);
     }
 }
