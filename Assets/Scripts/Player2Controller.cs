53cc00ff53f830361adc84cb651e2535be7f01b1
Mehdi Afshari <miladafshari.06@gmail.com>
2025-05-16 18:13:12 +0330

    some animations with messy scripts

--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,18 +6,27 @@ public class Player2Controller : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
-    private Vector2 movingInput;
-    
-    private bool isGrounded = false;
+    public Vector2 movingInput;
+
+    private bool isGrounded;
     [SerializeField] private Transform groundCheckCollider;
-    [SerializeField] private const float groundCheckRadius = 0.2f;
+    private const float groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask groundLayer;
-    
+
+    private int jumpCount;
+    private const int maxJumps = 2;
+
     Animator animator;
+    private Rigidbody2D rb;
     
+    [SerializeField] private GameObject arrow;
+    private bool canShoot = true;
+
     void Awake()
     {
+        isGrounded = false;
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -26,27 +36,81 @@ public class Player2Controller : MonoBehaviour
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded)
+        if (context.performed && jumpCount < maxJumps)
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumpCount++;
+
             animator.SetTrigger("Jump");
         }
     }
 
+    public void OnShot(InputAction.CallbackContext context)
+    {
+        if (context.performed && canShoot)
+        {
+            canShoot = false; // block further shots
+            animator.SetTrigger("Shot");
+            StartCoroutine(DelayedArrowShot(0.6f)); // delay to shoot
+            StartCoroutine(ResetShotCooldown(1.2f)); // total animation duration (adjust as needed)
+        }
+    }
+
+
+    private IEnumerator DelayedArrowShot(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+
+        float direction = Mathf.Sign(transform.localScale.x); // +1 for right, -1 for left
+
+        Vector3 offset = new Vector3(1.5f * direction, 0.15f, 0f); // relative to player
+        Vector3 spawnPos = transform.position + offset;
+
+        GameObject newArrow = Instantiate(arrow, spawnPos, Quaternion.identity);
+
+        // Flip arrow to match direction
+        Vector3 arrowScale = newArrow.transform.localScale;
+        arrowScale.x = Mathf.Abs(arrowScale.x) * direction;
+        newArrow.transform.localScale = arrowScale;
+    }
+
+    private IEnumerator ResetShotCooldown(float cooldownTime)
+    {
+        yield return new WaitForSeconds(cooldownTime);
+        canShoot = true;
+    }
+    
     public void groundCheck()
     {
         isGrounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(
+            groundCheckCollider.position, groundCheckRadius, groundLayer
+        );
+
+        foreach (Collider2D col in colliders)
+        {
+            if (col.CompareTag("Player1") || ((1 << col.gameObject.layer) & groundLayer) != 0)
+            {
+                isGrounded = true;
+                break;
+            }
+        }
         
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
-        if (colliders.Length > 0) isGrounded = true;
+        if (isGrounded && rb.linearVelocity.y <= 0.01f)
+        {
+            jumpCount = 0;
+        }
     }
 
-    void Update()
+
+    void FixedUpdate()
     {
         groundCheck();
         move();
     }
-    
+
     private void move()
     {
         if (movingInput.x > 0)
@@ -57,11 +121,10 @@ public class Player2Controller : MonoBehaviour
         {
             transform.localScale = new Vector3(-2, 2, 2);
         }
-        
+
         bool isRunning = Mathf.Abs(movingInput.x) > 0.01f;
         animator.SetBool("IsRun", isRunning);
-        
-        
+
         Vector2 move = new Vector2(movingInput.x, 0) * (speed * Time.deltaTime);
         transform.Translate(move);
     }
