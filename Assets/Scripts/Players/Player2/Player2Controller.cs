9a2d9c7e3a3e46b302f0dc4933addbb7abd1f2a4
Mehdi Afshari <miladafshari.06@gmail.com>
2025-06-01 17:08:37 +0330

    commit for checkout

--- Assets/Scripts/Players/Player2/Player2Controller.cs
+++ Assets/Scripts/Players/Player2/Player2Controller.cs
@@ -47,7 +47,6 @@ public class Player2Controller : BaseControll
         {
             animator.SetTrigger("Shoot");
             
-            StartCoroutine(AllowShootingAgainAfter(1f));
         }
     }
 
