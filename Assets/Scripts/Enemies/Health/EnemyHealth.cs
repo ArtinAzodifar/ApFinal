871ca2d782b2861916a07954a9c1ad3e7b824a43
ArtinAzd <artinazod85@gmail.com>
2025-08-07 17:05:44 +0330

    enemy health started

--- Assets/Scripts/Enemies/Health/EnemyHealth.cs
+++ Assets/Scripts/Enemies/Health/EnemyHealth.cs
@@ -1,98 +1,160 @@
 using System;
 using UnityEngine;
+using Unity.Netcode;
+using System.Collections;
 
-public class EnemyHealth : MonoBehaviour, Damagable
+public class EnemyHealth : NetworkBehaviour, Damagable
 {
     [SerializeField] private GameObject[] collectibles;
-    [SerializeField] private int health;
+    [SerializeField] private int initialHealth;
+    private NetworkVariable<int> health = new NetworkVariable<int>();
     [SerializeField] private EnemyHB healthBar;
     public bool isDead = false;
-    
+
     private SoundPlayer soundPlayer;
     [SerializeField] private string deathSoundName;
 
     private DamageFlash _damageFlash;
+    private GameManager gameManager;
+
+    public void Awake()
+    {
+        gameManager = GameManager.Instance;
+    }
 
 
     public void Start()
     {
-        if (healthBar != null)
-        {
-            healthBar.SetMaxHealth(health);   
-        }
+        if (gameManager.IsLocalMode() || IsServer) health.Value = initialHealth;
+        if (healthBar != null) healthBar.SetMaxHealth(initialHealth);
         soundPlayer = GetComponent<SoundPlayer>();
         _damageFlash = GetComponent<DamageFlash>();
     }
 
     public void Damage(int amount)
     {
-        health -= amount;
-        if(_damageFlash != null)    _damageFlash.CallDamageFlash();
-        
+        if (!gameManager.IsLocalMode() && !IsServer) return;
+
+        health.Value -= amount;
+
+        //damage animation
+        if (_damageFlash != null)
+        {
+            if (gameManager.IsLocalMode()) _damageFlash.CallDamageFlash();
+            else if (IsServer) damageFlashClientRpc();
+        }
+
+        //health bar update
         if (healthBar != null)
         {
-            healthBar.SetHealth(health);
+            if (gameManager.IsLocalMode()) healthBar.SetHealth(health.Value);
+            else if (IsServer) updateHealthBarClientRpc(health.Value);
         }
-        if (health <= 0 && !isDead)
+
+        //death check
+        if (health.Value <= 0 && !isDead)
         {
             isDead = true;
             Animator animator = gameObject.GetComponent<Animator>();
-            
+
+            //sound
             if (soundPlayer != null)
             {
-                AudioClip clipToPlay = soundPlayer.GetClipByName(deathSoundName);
-
-                if (clipToPlay != null)
-                {
-                    float volume = AudioController.Instance.sfxVolume;
-                    AudioSource.PlayClipAtPoint(clipToPlay, transform.position, volume);
-                }
-                else
-                {
-                    Debug.LogError($"SOUND NOT FOUND! The name '{deathSoundName}' does not match any clip in the SoundPlayer component on {gameObject.name}.");
-                }
+                if (gameManager.IsLocalMode()) playTheSound();
+                else if (IsServer) PlaySoundClientRpc();
             }
-            
+
+            //random collectible create
+            //random < 25 -> healthPoint 25%
+            //random < 50 -> damage booster 25%
+            //random < 60 -> max mana 10%
+            //random > 60 -> nothing 40%
             int random = UnityEngine.Random.Range(0, 100);
-            Debug.Log(random);
             if (random < 25)
             {
                 Vector3 position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-                Instantiate(collectibles[0], position, Quaternion.identity);//health
-            } 
+                var collectible = Instantiate(collectibles[0], position, Quaternion.identity);//health
+
+                //for online mode
+                var netObject = collectible.GetComponent<NetworkObject>();
+                if (!gameManager.IsLocalMode() && IsServer && netObject != null) netObject.Spawn();
+            }
             else if (random < 50)
             {
                 Vector3 position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-                Instantiate(collectibles[1], position, Quaternion.identity);//damage booster
+                var collectible = Instantiate(collectibles[1], position, Quaternion.identity);//damage booster
+
+                //for online mode
+                var netObject = collectible.GetComponent<NetworkObject>();
+                if (!gameManager.IsLocalMode() && IsServer && netObject != null) netObject.Spawn();
             }
             else if (random < 60)
             {
                 Vector3 position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-                Instantiate(collectibles[2], position, Quaternion.identity);//max mana
+                var collectible = Instantiate(collectibles[2], position, Quaternion.identity);//max mana
+
+                //for online mode
+                var netObject = collectible.GetComponent<NetworkObject>();
+                if (!gameManager.IsLocalMode() && IsServer && netObject != null) netObject.Spawn();
             }
+
             if (animator != null && HasTrigger(animator, "Death"))
             {
                 animator.SetTrigger("Death");
-                if(GetComponent<PersistentObject>() != null)    GetComponent<PersistentObject>().OnProcessed();
-                Destroy(gameObject, 1.8f);
+
+                //save in localmode
+                if (gameManager.IsLocalMode() && GetComponent<PersistentObject>() != null) GetComponent<PersistentObject>().OnProcessed();
+
+                //delete object
+                if (gameManager.IsLocalMode()) Destroy(gameObject, 1.8f);
+                else if (IsServer) StartCoroutine(Despawn());
             }
             else
             {
-                if(GetComponent<PersistentObject>() != null)    GetComponent<PersistentObject>().OnProcessed();
-                Destroy(gameObject);   
+                //save in local mode
+                if (gameManager.IsLocalMode() && GetComponent<PersistentObject>() != null) GetComponent<PersistentObject>().OnProcessed();
+
+                //delete object
+                if (gameManager.IsLocalMode()) Destroy(gameObject);
+                else if (IsServer) gameObject.GetComponent<NetworkObject>().Despawn();
             }
         }
     }
-    
+
+    private void playTheSound()
+    {
+        AudioClip clipToPlay = soundPlayer.GetClipByName(deathSoundName);
+
+        if (clipToPlay != null)
+        {
+            float volume = AudioController.Instance.sfxVolume;
+            AudioSource.PlayClipAtPoint(clipToPlay, transform.position, volume);
+        }
+        else Debug.LogError($"SOUND NOT FOUND! The name '{deathSoundName}' does not match any clip in the SoundPlayer component on {gameObject.name}.");
+    }
+
     private bool HasTrigger(Animator animator, string triggerName)
     {
         foreach (AnimatorControllerParameter param in animator.parameters)
         {
-            if (param.type == AnimatorControllerParameterType.Trigger && param.name == triggerName)
-            {
-                return true;
-            }
+            if (param.type == AnimatorControllerParameterType.Trigger && param.name == triggerName) return true;
         }
         return false;
     }
+
+    private IEnumerator Despawn()
+    {
+        yield return new WaitForSeconds(1.8f);
+        gameObject.GetComponent<NetworkObject>().Despawn();
+    }
+
+    //ClientRpc
+    [ClientRpc]
+    private void updateHealthBarClientRpc(int amount) { healthBar.SetHealth(amount); }
+
+    [ClientRpc]
+    private void PlaySoundClientRpc() { playTheSound(); }
+
+    [ClientRpc]
+    private void damageFlashClientRpc() { _damageFlash.CallDamageFlash(); }
 }
