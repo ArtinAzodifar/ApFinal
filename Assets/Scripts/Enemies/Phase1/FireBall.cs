b50917a2522fc441b03d7ba67fd35cf1be094753
ArtinAzd <artinazod85@gmail.com>
2025-08-09 16:31:49 +0330

    flying demon finished

--- Assets/Scripts/Enemies/Phase1/FireBall.cs
+++ Assets/Scripts/Enemies/Phase1/FireBall.cs
@@ -1,26 +1,33 @@
 using System;
 using Unity.Cinemachine;
 using UnityEngine;
+using Unity.Netcode;
 
-public class FireBall : MonoBehaviour
+public class FireBall : NetworkBehaviour
 {
     [SerializeField] private int damageAmount;
     [SerializeField] private float speed;
     private Rigidbody2D rb;
     private Vector3 startPos;
+    private GameManager gameManager;
 
     public void Awake()
     {
+        gameManager = GameManager.Instance;
         rb = GetComponent<Rigidbody2D>();
     }
 
     public void OnEnable()
     {
+        if (!gameManager.IsLocalMode() && !IsServer) return;
+        
         startPos = transform.position;
     }
 
     public void Update()
     {
+        if (!gameManager.IsLocalMode() && !IsServer) return;
+
         rb.linearVelocity = Vector2.left * transform.localScale.x * speed;
         float xDistance = startPos.x - transform.position.x;
         if (Mathf.Abs(xDistance) >= 30)
@@ -31,6 +38,8 @@ public class FireBall : MonoBehaviour
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
+        if (!gameManager.IsLocalMode() && !IsServer) return;
+
         if (collision.CompareTag("Player1") || collision.CompareTag("Player2"))
         {
             if (collision.GetComponent<Damagable>() != null)
